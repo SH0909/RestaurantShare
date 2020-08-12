38a15e207b077bc429195dcd5cc7bf1d052656de
SH0909 <poa0909@gmail.com>
2020-08-12 22:34:45 +0900

    url&template setting

---
+++ shareRes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request,'shareRes/index.html')
+
+def restaurantDetail(request):
+    return render(request,'shareRes/restaurantDetail.html')
+
+def restaurantCreate(request):
+    return render(request,'shareRes/restaurantCreate.html')
+
+def categoryCreate(request):
+    return render(request,'shareRes/categoryCreate.html')
